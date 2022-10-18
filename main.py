7be88ba3c68b142820213189b32d297c11c0d2e0
Theo Morvan <theo.morvan@hec.edu>
2022-10-18 14:37:05 +0000

    main file modified to train with hfactory gpus

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ import ipdb
 
 
 general_path = os.path.dirname(os.getcwd())
-path_data = os.path.join(general_path, "naf_activite.csv")
-path_mapping = os.path.join(general_path, "naf_mapping.csv")
-EPOCHS = 1
-LR = 1e-6
+path_data = os.path.join(os.getcwd(),"data","raw", "naf_activite.csv")
+path_mapping = os.path.join(os.getcwd(),"data","raw", "naf_mapping.csv")
+EPOCHS = 5
+LR = 1e-5
 
 if __name__ == "__main__":
     df = pd.read_csv(path_data, delimiter="|").dropna().drop(columns=["Unnamed: 0"])
     df_mapping = pd.read_csv(path_mapping, delimiter=";", encoding="latin-1")
     df_mapping.rename(columns={"naf5": "NAF_CODE"}, inplace=True)
     df_merged = df.merge(df_mapping, on="NAF_CODE", how="inner")
-    idxes = np.random.choice(np.arange(len(df_merged)), 30_000)
+    idxes = np.random.choice(np.arange(len(df_merged)), 50_000)
     df_merged = df_merged.iloc[idxes, :]
     model = "camembert-base"
     tokenizer = AutoTokenizer.from_pretrained(model)
