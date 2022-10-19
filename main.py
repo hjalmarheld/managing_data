aa2aba9e03848c589648e9f071cea23353e07264
Windu12345 <32morvantheo@gmail.com>
2022-10-19 18:45:11 +0200

    adapting code to config file + properly saving predictions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cProfile import label
+from matplotlib.backend_bases import key_press_handler
 import pandas as pd
 import torch
 import numpy as np
@@ -10,17 +12,18 @@ from tqdm import tqdm
 import os
 from function_camembert_model import *
 import ipdb
-
+from config import *
 
 general_path = os.path.dirname(os.getcwd())
 name_file = "labelled articles cleaned.csv"
 path_data = os.path.join(os.getcwd(),"data","external", name_file)
 path_mapping = os.path.join(os.getcwd(),"data","raw", "naf_mapping.csv")
 path_predictions = os.path.join(os.getcwd(),"data","test.csv")
-EPOCHS = 6
+EPOCHS = 2
 LR = 1e-5
 save = True
 increase_data = False
+batch_size=4
 
 if __name__ == "__main__":
     df = pd.read_csv(path_data, delimiter=",").dropna().drop(columns=["Unnamed: 0"])
@@ -36,7 +39,7 @@ if __name__ == "__main__":
         df_merged_bis.columns = ["company","text","title","naf"]
         df_merged = pd.concat([df_merged, df_merged_bis[df_merged.columns]])
         
-    idxes = np.random.choice(np.arange(len(df_merged)), 50_000)
+    idxes = np.random.choice(np.arange(len(df_merged)), 400)
     df_merged = df_merged.iloc[idxes, :]
     model = "camembert-base"
     tokenizer = AutoTokenizer.from_pretrained(model)
@@ -56,12 +59,22 @@ if __name__ == "__main__":
     model = CamemBertClassifierShort(number_labels=len(labels_list))
     train_data = Dataset(df_train, tokenizer, labels=labels)
     
-    train(model, df_train, df_val, LR, EPOCHS, tokenizer, labels,use_samplers=True)
+    train(model, df_train, df_val, LR, EPOCHS, tokenizer, labels,use_samplers=True,batch_size=batch_size)
     acc_test, predictions = evaluate(model, df_test,tokenizer, labels=labels)
     df_test_final = pd.read_csv(path_predictions)
     df_test_final["naf"] = df_train["naf"].iloc[0]
 
-    predictions = predict(model,df_test_final, tokenizer, labels)
+    predictions = predict(model,df_test_final, tokenizer, labels, batch_size)
+    values = []
+    for i in range(len(predictions)):
+        for j in range(predictions[i].shape[0]):
+            values.append(list(predictions[i][j]))
+    df_predictions = pd.DataFrame(values)
+    df_predictions.columns = [f"naf_code = {naf_code}" for naf_code in labels.keys()]
+
     if save:
-        np.savetxt('predictions.csv', np.array(predictions), delimiter=',')
+        try:
+            df_predictions.to_csv('predictions.csv',index=False)
+        except:
+            ipdb.set_trace()
     ipdb.set_trace()
