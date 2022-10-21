66b36398bf6c86c83d17dc04af925abf535cbb68
Hjalmar Held <hjalmarheld@gmail.com>
2022-10-21 10:23:29 +0200

    config file

---
+++ config.py
@@ -0,0 +1,11 @@
+EPOCHS = 4
+LR = 1e-5
+save = True
+name_saving = "final_predictions.csv"
+increase_data=False
+batch_size=32
+text_column="combined"
+test_column="text"
+size_training=200_000
+column_labels="naf"
+max_length = 512
